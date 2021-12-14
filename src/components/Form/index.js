8c96f43fefd7547c93ff861fbb1aea5687e4bbcf
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-12-14 15:18:14 -0400

    create: component Form

---
+++ src/components/Form/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { TextInput, Text, View, Button } from "react-native"
+
+const Form = () => {
+    return (
+        <View>
+            <View>
+
+                <Text>Altura</Text>
+                <TextInput
+                    placeholder="Ex.: 1.75"
+                    keyboardType="numeric"
+                />
+
+                <Text>Peso</Text>
+                <TextInput
+                    placeholder="Ex.: 75.256"
+                    keyboardType="numeric"
+                />
+
+                <Button title="Calcular IMC" />
+
+            </View>
+
+            {/* <ResultImc /> */}
+
+        </View>
+    )
+}
+
+export default Form
